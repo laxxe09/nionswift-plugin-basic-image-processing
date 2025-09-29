d3131f8449509fd2ac2ee95c5c72e549cdd470ad
laxxe09 <cmsacosta7@outlook.com>
2025-09-29 14:07:53 -0700

    Removed nonexistent MenuItem

--- nionswift_plugin/basic_image_processing_ui/__init__.py
+++ nionswift_plugin/basic_image_processing_ui/__init__.py
@@ -1,5 +1,4 @@
 # import components so they get registered with Nion Swift
-from nionswift_plugin.basic_image_processing_ui import MenuItem
 from nionswift_plugin.basic_image_processing_ui.BasicImageProcessingPanel import run
 
 # Register the panel when the plugin is loaded
